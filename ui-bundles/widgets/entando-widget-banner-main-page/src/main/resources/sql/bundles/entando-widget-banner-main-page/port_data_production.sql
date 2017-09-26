9981ee8f86530e56655187f708a8dce6927a81b3
D. Minnai <d.minnai@entando.com>
2017-09-26 11:17:01 +0200

    banner main purple refactor

--- ui-bundles/widgets/entando-widget-banner-main-page/src/main/resources/sql/bundles/entando-widget-banner-main-page/port_data_production.sql
+++ ui-bundles/widgets/entando-widget-banner-main-page/src/main/resources/sql/bundles/entando-widget-banner-main-page/port_data_production.sql
@@ -1,7 +1,7 @@
 INSERT INTO widgetcatalog (code, titles, parameters, plugincode, parenttypecode, defaultconfig, locked, maingroup) VALUES ('entando-widget-banner-main-page', '<?xml version="1.0" encoding="UTF-8"?>
 <properties>
-    <property key="en">entando-widget-banner-main-page</property>
-    <property key="it">entando-widget-banner-main-page </property>
+    <property key="en">Main banner Purple</property>
+    <property key="it">Main banner Purple</property>
 </properties>', NULL, NULL, NULL, NULL, 1, NULL);
 
 INSERT INTO guifragment (code, widgettypecode, plugincode, gui, defaultgui, locked) VALUES ('entando-widget-banner-main-page', 'entando-widget-banner-main-page', NULL, NULL, '<#assign wp=JspTaglibs["/aps-core"]>
