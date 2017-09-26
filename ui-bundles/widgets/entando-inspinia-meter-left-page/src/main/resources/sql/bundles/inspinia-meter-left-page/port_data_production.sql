9d068ad6d81adda8f901b281af5ede14d193956e
D. Minnai <d.minnai@entando.com>
2017-09-26 12:53:30 +0200

    meters refactor

--- ui-bundles/widgets/entando-inspinia-meter-left-page/src/main/resources/sql/bundles/inspinia-meter-left-page/port_data_production.sql
+++ ui-bundles/widgets/entando-inspinia-meter-left-page/src/main/resources/sql/bundles/inspinia-meter-left-page/port_data_production.sql
@@ -1,7 +1,7 @@
 INSERT INTO widgetcatalog (code, titles, parameters, plugincode, parenttypecode, defaultconfig, locked, maingroup) VALUES ('entando-inspinia-meter-left-page', '<?xml version="1.0" encoding="UTF-8"?>
 <properties>
-<property key="en">entando-inspinia-meter-left-page</property>
-<property key="it">entando-inspinia-meter-left-page</property>
+<property key="en">meter left Inspinia</property>
+<property key="it">meter left Inspinia</property>
 </properties>', NULL, NULL, NULL, NULL, 1, NULL);
 
 INSERT INTO guifragment (code, widgettypecode, plugincode, gui, defaultgui, locked)
--- ui-bundles/widgets/entando-inspinia-meter-right-page/src/main/resources/sql/bundles/inspinia-meter-right-page/port_data_production.sql
+++ ui-bundles/widgets/entando-inspinia-meter-right-page/src/main/resources/sql/bundles/inspinia-meter-right-page/port_data_production.sql
@@ -1,7 +1,7 @@
 INSERT INTO widgetcatalog (code, titles, parameters, plugincode, parenttypecode, defaultconfig, locked, maingroup) VALUES ('entando-inspinia-meter-right-page', '<?xml version="1.0" encoding="UTF-8"?>
 <properties>
-<property key="en">entando-inspinia-meter-right-page</property>
-<property key="it">entando-inspinia-meter-right-page</property>
+<property key="en">meter right Inspinia</property>
+<property key="it">meter right Inspinia</property>
 </properties>', NULL, NULL, NULL, NULL, 1, NULL);
 
 INSERT INTO guifragment (code, widgettypecode, plugincode, gui, defaultgui, locked)
