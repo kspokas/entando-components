59abbe062b96d317786612d6ef40809ccb16f269
D. Minnai <d.minnai@entando.com>
2017-10-09 15:37:56 +0200

    entando-widget-lender added

--- ui-bundles/widgets/entando-widget-lender-login/src/main/resources/sql/bundles/entando-widget-lender-login/port_data_production.sql
+++ ui-bundles/widgets/entando-widget-lender-login/src/main/resources/sql/bundles/entando-widget-lender-login/port_data_production.sql
@@ -1,7 +1,7 @@
 INSERT INTO widgetcatalog (code, titles, parameters, plugincode, parenttypecode, defaultconfig, locked, maingroup) VALUES ('entando-widget-lender-login', '<?xml version="1.0" encoding="UTF-8"?>
 <properties>
-<property key="en">Choose a language Inspinia</property>
-<property key="it">Scegli la lingua Inspinia</property>
+<property key="en">Lender login </property>
+<property key="it">Login del lender</property>
 </properties>', NULL, NULL, NULL, NULL, 1, NULL);
 
 INSERT INTO guifragment (code, widgettypecode, plugincode, gui, defaultgui, locked) VALUES ('entando-widget-lender-login', 'entando-widget-lender-login', NULL, NULL, '<#assign wp=JspTaglibs["/aps-core"]>
